c40af64dabb9d8e71aa91a7adac617eb12f9c2db
Lukiam23 <mscorreia95@gmail.com>
2022-01-08 18:58:25 -0300

    grid content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Card from './components/Card'
 import { Helmet } from 'react-helmet';
 import Header from './components/Header'
 import MenuLateral from './components/MenuLateral'
-import styles from './components/MenuLateral.module.css'
 
 function App() {
   return (
@@ -16,8 +15,54 @@ function App() {
       <MenuLateral />
       <Header img='images/pokeball.png' alt='pokeball'/>
 
-      <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/pikachu.png' alt='Pikachu'/>
-      
+      <div className="cardContainer">
+        <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+        <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+        
+        <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+<Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' qt='1' img='images/p2.png' alt='Pikachu'/>
+        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' qt='1' img='images/p3.png' alt='Articuno'/>
+        <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' qt='10' img='images/p1.png' alt='Clefairy'/>
+
+
+      </div>
+
     </div>
   );
 }
