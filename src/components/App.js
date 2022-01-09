b6ee734513c7febe87879f1e63b2ad501dc31b86
Lukiam23 <mscorreia95@gmail.com>
2022-01-09 17:03:14 -0300

    cards responsivos

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Card from './components/Card'
+import styles from '../css/App.module.css';
+import Card from './Card'
 import { Helmet } from 'react-helmet';
-import Header from './components/Header'
-import MenuLateral from './components/MenuLateral'
+import Header from './Header'
+import MenuLateral from './MenuLateral'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Helmet>
         <script src="https://kit.fontawesome.com/3475a922f1.js" crossorigin="anonymous"></script>
         <title>RocketMarket</title>
@@ -15,7 +15,7 @@ function App() {
       <MenuLateral />
       <Header img='images/pokeball.png' alt='pokeball'/>
 
-      <div className="cardContainer">
+      <div className={styles.cardContainer}>
         <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' img='images/p2.png' alt='Pikachu'/>
         <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' img='images/p3.png' alt='Articuno'/>
         <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' img='images/p1.png' alt='Clefairy'/>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import styles from '../css/Card.module.css'
 function Card({nome, tipo, preco, img, alt}) {
 	return (
-			<div className={styles.cardContainers}>
-				<img src={img} alt={alt} />
+	<div className={styles.cardContainers}>
+		<img src={img} alt={alt} />
 
-				<div className={styles.nomePreco}>
-					<label>{nome}</label>
-					<label>{preco}</label>
-				</div>
+		<div className={styles.nomePreco}>
+			<label>{nome}</label>
+			<label>{preco}</label>
+		</div>
 
-				<div className={styles.descricao}>
-					<label>O {nome} é um pokémon do tipo {tipo}</label>
-				</div>			
-			</div>
+		<div className={styles.descricao}>
+			<label>O {nome} é um pokémon do tipo {tipo}</label>
+	</div>			
+	</div>
 	);
 }
 
