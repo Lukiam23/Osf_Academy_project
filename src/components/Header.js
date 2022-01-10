5ac8d16ae58b004b4fc73f7ff037e9c97482b08d
Lukiam23 <mscorreia95@gmail.com>
2022-01-09 23:28:56 -0300

    seach button checkbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,21 @@ import styles from '../css/Header.module.css'
 
 function Header({img,alt}) {
 	return (
+		
 		<div className={styles.headerContainer}>
+			<input type="checkbox" id={styles.check}/>
 			<h1>Rocket<span>Market</span></h1>
 			<div className={styles.searchBox}>
 				<input type="text" placeholder="Insira um pokémon" className={styles.headerSearchInput}/>
-				<a className={styles.searchButton} href="#">
-					<i class="fas fa-search"></i>
+				<a className={styles.searchButton}  href="#">
+					<label for={styles.check}>
+						<i class="fas fa-search"></i>
+					</label>
 				</a>
 			</div>
 			
 		</div>
+		
 	);
 }
 
