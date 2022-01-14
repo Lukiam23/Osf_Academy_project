52273cf1154c379f243250964761cdcd40166252
Lukiam23 <mscorreia95@gmail.com>
2022-01-13 23:59:21 -0300

    '

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,17 @@ import Card from './Card'
 import { Helmet } from 'react-helmet';
 import Header from './Header'
 import MenuLateral from './MenuLateral'
+import axios from 'axios'
+
+
 
 function App() {
+
+  let datas = [ {"nome":"Pikachu", "tipo":"Elétrico", "preco":"R$ 10.000", "img":"images/p2.png", "alt":"Pikachu"},
+        { "nome":"Articuno", "tipo":"Gelo Voador", "preco":"R$ 21.000", "img":"images/p3.png", "alt":"Articuno"}]
+
+  const nome = 'Matheus'
+
   return (
     <div className={styles.App}>
       <Helmet>
@@ -16,8 +25,9 @@ function App() {
       <Header img='images/pokeball.png' alt='pokeball'/>
 
       <div className={styles.cardContainer}>
-        <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' img='images/p2.png' alt='Pikachu'/>
-        <Card nome='Articuno' tipo='Gelo Voador' preco='R$ 21.000' img='images/p3.png' alt='Articuno'/>
+        {this.datas.map( data =>
+            {return(<h1>{data.nome}</h1>); })}
+        
         <Card nome='Clefairy' tipo='Fada' preco='R$ 2.000' img='images/p1.png' alt='Clefairy'/>
 
         <Card nome='Pikachu' tipo='Elétrico' preco='R$ 10.000' img='images/p2.png' alt='Pikachu'/>
