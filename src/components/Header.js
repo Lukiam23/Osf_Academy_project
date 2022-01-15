c693d4c05f3d2921f56aa2faf9662c71cd0cbea6
Lukiam23 <mscorreia95@gmail.com>
2022-01-14 21:58:41 -0300

    input seach

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,52 @@
+import React from "react";
 import styles from '../css/Header.module.css'
 
+
+
 function Header({img,alt}) {
+	const [show, setShow] = React.useState(null);
+
+	const options = () =>{
+		let divInput = document.getElementById('headerSearchInput');
+
+		if(divInput.value){
+			setShow(true);
+		} else {
+			setShow(false)
+		}		
+	}
+
+	const visibleBox = {
+		padding: "0px 20px 20px 20px",
+
+		"border-radius": "10px 10px",
+		"box-shadow": "0px 4px 8px 0px rgba(1,0,0,.8)", 
+		height: "200px",
+		background: "white"
+	};
+
+	const visibleOptions = {
+		"visibility": "visible",
+	};
+
 	return (
 		
 		<div className={styles.headerContainer}>
 			<input type="checkbox" id={styles.check}/>
 			<h1>Rocket<span>Market</span></h1>
 			<div className={styles.searchBox}>
-				<input type="text" placeholder="Insira um pokémon" className={styles.headerSearchInput}/>
+				<div id="box" className={styles.inputBox} style={ show ? visibleBox : {}}>
+					<input type="text" id="headerSearchInput" onChange={options} placeholder="Insira um pokémon" className={styles.headerSearchInput}/>
+					<div id="options" style={ show ? visibleOptions : {}} className={styles.options}>
+					</div>
+				</div>
 				<a className={styles.searchButton}  href="#">
 					<label for={styles.check}>
 						<i class="fas fa-search"></i>
 					</label>
 				</a>
+				
+
 			</div>
 			
 		</div>
