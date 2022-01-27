0d05601c2c84d3022d7782665766c69263a39ada
Lukiam23 <mscorreia95@gmail.com>
2022-01-27 00:15:20 -0300

    incrementar quantidade carrinho

--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,12 +1,26 @@
 import styles from '../css/CarItem.module.css';
+import { useState } from "react";
 
 export default function CarItem({pokemon, deleteItem}){
+	const [quantidade,setQuantidade] = useState(1);
 	const deleteItemCar = () => deleteItem(pokemon);
+	const addItemCar = () => {if(pokemon.qt < quantidade){setQuantidade(quantidade + 1)}};
+	const removeItemCar = () => {if(quantidade > 1){setQuantidade(quantidade - 1)}};
+
+	const limitColor ={
+		'color': 'grey'
+	} 
+
+
+
 	return (
 		<div className={styles.carItem}>
 			<img src={pokemon.img} alt={pokemon.alt} />
 			<div className={styles.nome}>{pokemon.nome}</div>
-			<span onClick={deleteItemCar}><i class="fas fa-trash-alt"></i></span>
+			<div className={styles.minusButton} onClick={removeItemCar}><span style={ (quantidade === 1) ? limitColor : {}}><i class="fas fa-minus"></i></span></div>
+			<div className={styles.quantidade}>{quantidade}</div>
+			<div className={styles.plusButton} onClick={addItemCar}><span style={ quantidade === pokemon.qt ? limitColor : {}}><i class="fas fa-plus"></i></span></div>
+			<div className={styles.deleteButton}><span onClick={deleteItemCar}><i class="fas fa-trash-alt"></i></span></div>
 		</div>
 	);
 }
