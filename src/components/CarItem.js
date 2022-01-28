95010bb205ec99da1d0c0df93519fc02f65d4098
Lukiam23 <mscorreia95@gmail.com>
2022-01-28 02:41:14 -0300

    finalização do carro

--- src/components/CarItem.js
+++ src/components/CarItem.js
@@ -1,11 +1,7 @@
 import styles from '../css/CarItem.module.css';
 import { useState, useEffect } from "react";
-import {useAppContext} from '../components/AppContextProvider'
 
-export default function CarItem({pokemon, deleteItem}){
-	const {carList, setCarList} = useAppContext();
-	const [quantidade, setQuantidade] = useState(pokemon.carQt)
-	
+export default function CarItem({pokemon, deleteItem, total, setTotal, setUpdateFlag, updateFlag}){	
 	const deleteItemCar = () => {
 		deleteItem(pokemon)
 	};
@@ -13,14 +9,14 @@ export default function CarItem({pokemon, deleteItem}){
 	const addItemCar = () => {
 		if(pokemon.carQt < pokemon.qt){
 			pokemon.carQt++;
-			setQuantidade(pokemon.carQt)
+			setUpdateFlag(!updateFlag)
 		}
 	};
 
 	const removeItemCar = () => {
 		if(pokemon.carQt > 1){
 			pokemon.carQt--;
-			setQuantidade(pokemon.carQt)
+			setUpdateFlag(!updateFlag)
 		}
 	};
 
@@ -34,7 +30,7 @@ export default function CarItem({pokemon, deleteItem}){
 			<div className={styles.nome}>{pokemon.nome}</div>
 			<div className={styles.preco}>R$ {pokemon.preco * pokemon.carQt}</div>
 			<div className={styles.minusButton} onClick={removeItemCar}><span style={ (pokemon.carQt === 1) ? limitColor : {}}><i class="fas fa-minus"></i></span></div>
-			<div className={styles.quantidade}>{quantidade}</div>
+			<div className={styles.quantidade}>{pokemon.carQt}</div>
 			<div className={styles.plusButton} onClick={addItemCar}><span style={ pokemon.carQt === pokemon.qt ? limitColor : {}}><i class="fas fa-plus"></i></span></div>
 			<div className={styles.deleteButton}><span onClick={deleteItemCar}><i class="fas fa-trash-alt"></i></span></div>
 		</div>
--- src/pages/CarPage.js
+++ src/pages/CarPage.js
@@ -6,14 +6,18 @@ import { useState, useEffect } from "react";
 
 function CarPage({texto}) {
 	const {carList, setCarList} = useAppContext();
-	const [total,setTotal] = useState(carList.reduce((acumulador, pokemon) => {return acumulador + (pokemon.preco *pokemon.carQt)},0));
+	
+	const [updateFlag,setUpdateFlag] = useState(true);
+	
+	const [total,setTotal] = useState();
+	
 	const deleteItem = (pokemon) => {
 		setCarList(carList.filter(item => item.nome !== pokemon.nome));
 	};	
 
 	useEffect(() => {
 		setTotal(carList.reduce((acumulador, pokemon) => {return acumulador + (pokemon.preco *pokemon.carQt)},0))
-	},[carList])
+	},[carList,updateFlag])
 
 	if (carList.length === 0){
 		return(
@@ -25,9 +29,13 @@ function CarPage({texto}) {
 
 	return (
 		<div className={styles.carContainer}>  
-		  <h1>Total: {total} </h1>
+		  <h1>
+		  Total: {carList.reduce((acumulador, pokemon) => {
+		  	return acumulador + (pokemon.preco *pokemon.carQt)
+		  },0)} 
+		  </h1>
           {carList.map(pokemon => {
-          	return(<CarItem pokemon={pokemon} deleteItem={deleteItem}/>)
+          	return(<CarItem pokemon={pokemon} setUpdateFlag={setUpdateFlag} updateFlag={updateFlag} deleteItem={deleteItem} setTotal={setTotal}/>)
           })}
         </div>
 	);
